0c81eede5bf9b1ca3b8c31dd6ef4149c5b3307b4
bRIMOs88 <bourbia.brahim@gmail.com>
2018-10-13 16:09:14 +0100

    first commit webpack samle

---
+++ src/main.js
@@ -0,0 +1,1 @@
+alert("Hello man dsds sss !");
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+var webpack = require("webpack");
+
+var path = require("path");
+
+module.exports = {
+
+    mode: 'development',
+    entry : './src/main.js',
+    output: {
+        
+        path: path.resolve(__dirname,"./dist"),
+
+        filename: 'bundle.js'
+
+    }
+
+};
