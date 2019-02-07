1d2a5bc1a3b12f35538a0676d8bcb161ced6d2e7
bRIMOs <brimos__@hotmail.com>
2019-02-07 16:22:50 +0100

    adding webpack

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
-//import './main.scss';
+import './main.scss';
+import Notification from './component/Notificiation'
 
 class App extends Component {
+  handleCLick  =  (e) => {
+    Notification.log(e);
+  };
   render() {
     return (
       <div className="App">
@@ -10,7 +14,7 @@ class App extends Component {
             Hello moto ! allah yer7am ...!ssss
             <br />test
         </header>
-
+        <button onClick={(e) => this.handleCLick(e)}>Click</button>
       </div>
     );
   }
